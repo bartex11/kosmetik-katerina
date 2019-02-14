02c7288023f7f52a3e3fe872bf52faf4f9ea4ea6
DOM\IvanVasilev <i.vasilev@spicymedia.io>
2019-02-14 18:02:03 +0100

    sw

--- sw.js
+++ sw.js
@@ -20,11 +20,9 @@ var urlsToCache = [
 
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -57,3 +55,21 @@ self.addEventListener('fetch', function(event) {
       })
     );
 });
+
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['katerina-kosmetik-v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
